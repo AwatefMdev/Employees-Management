b80097327e3a42dedb67fcba3d56ba60fea0ef59
AwatefMdev <riahi.awatef@gmail.com>
2019-06-10 21:25:44 +0000

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,13 +1,4 @@
-create table user (
-    email varchar(150) not null,
-    password char(64) not null,
-    id_employee integer (50),
-    constraint id_employee
-    foreign key (id) 
-     REFERENCES employee (id),
-     id_role integer REFERENCES role (id),
-    created_at timestamp default current_timestamp
-);
+
 
 create table employee (
     id serial primary key,
